b094b3274c4798decd454ad72205b551d69c3666
Leo Lee <nxl1969@gmail.com>
2017-01-28 11:13:20 -0500

    Switched to a class component from a functional component. Added a youtube search example outside of the class component.

--- video_browser/src/index.js
+++ video_browser/src/index.js
@@ -1,14 +1,24 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import SearchBar from './components/search_bar';
 
+import YTSearch from 'youtube-api-search';
+
 const API_KEY = 'AIzaSyDCDYzzx_qTJrXXesEwt0CLPeMvMK1jcW8';
-const App = () => {
-	return (
-		<div>
-			<SearchBar/>
-		</div>
-		);
+
+YTSearch({key:API_KEY, term:'surfboards'},function(data){
+	console.log(data);
+});
+
+
+class App extends Component{
+	render(){
+		return (
+			<div>
+				<SearchBar/>
+			</div>
+		)
+	}
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'));
