823fad0de0a955cded3bd2845251db172d7f3b53
Leo Lee <nxl1969@gmail.com>
2017-01-29 17:34:00 -0500

    Refactor the youtube search into its own method.

--- video_browser/src/index.js
+++ video_browser/src/index.js
@@ -16,7 +16,10 @@ class App extends Component{
 		this.state = {
 					 videos:[],
 					 selectedVideo:null};
-		YTSearch({key:API_KEY, term:'surfboards'},(videos) => {
+		this.videoSearch('surfboards');
+	}
+	videoSearch(term){
+		YTSearch({key:API_KEY, term:term},(videos) => {
 			this.setState({
 				videos:videos,
 				selectedVideo:videos[0]}); //ES6: this.setState({videos:videos})
@@ -25,7 +28,7 @@ class App extends Component{
 	render(){
 		return (
 			<div>
-				<SearchBar/>
+				<SearchBar onSearchTermChange = {term => this.videoSearch(term)}/>
 				<VideoDetail video={this.state.selectedVideo}/>
 				<VideoList 
 				  onVideoSelect = {selectedVideo => this.setState({selectedVideo})}
