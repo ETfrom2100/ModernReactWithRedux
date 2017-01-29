367a799f3518270e37e0e0cad6e838d6deb3f4a3
Leo Lee <nxl1969@gmail.com>
2017-01-29 01:18:57 -0500

    Add a selectedVideo state. Change the state  whenever a video list item is selected.

--- video_browser/src/index.js
+++ video_browser/src/index.js
@@ -13,17 +13,23 @@ const API_KEY = 'AIzaSyDCDYzzx_qTJrXXesEwt0CLPeMvMK1jcW8';
 class App extends Component{
 	constructor(props){
 		super(props);
-		this.state = {videos:[]};
+		this.state = {
+					 videos:[],
+					 selectedVideo:null};
 		YTSearch({key:API_KEY, term:'surfboards'},(videos) => {
-			this.setState({videos}); //ES6: this.setState({videos:videos})
+			this.setState({
+				videos:videos,
+				selectedVideo:videos[0]}); //ES6: this.setState({videos:videos})
 		});
 	}
 	render(){
 		return (
 			<div>
 				<SearchBar/>
-				<VideoDetail video={this.state.videos[0]}/>
-				<VideoList videos={this.state.videos}/>
+				<VideoDetail video={this.state.selectedVideo}/>
+				<VideoList 
+				  onVideoSelect = {selectedVideo => this.setState({selectedVideo})}
+				  videos={this.state.videos}/>
 			</div>
 		)
 	}
