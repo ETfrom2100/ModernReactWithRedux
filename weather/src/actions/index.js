294653e23abca4ea6a0204cd0a3c2754e0cbd8d0
Leo Lee <nxl1969@gmail.com>
2017-02-02 23:06:40 -0500

    Create a action creator fetchWeather. Action creator always return an object which contains a type and a payload. Use axios to make the ajax calls.

--- weather/src/actions/index.js
+++ weather/src/actions/index.js
@@ -1,1 +1,15 @@
-const API_key = '7ae59035c0dcb374826994830b6d6d11';
+import axios from 'axios';
+const API_KEY = '7ae59035c0dcb374826994830b6d6d11';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`; //ES6 syntax
+//action creator always return a action
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+export default function fetchWeather(city){
+	
+	const url = `${ROOT_URL}&q=${city},us`;
+	const request = axios.get(url);
+	return {
+		type:FETCH_WEATHER,
+		payload:request
+	}
+	
+}
