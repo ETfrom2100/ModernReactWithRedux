62dfb2af5215174b9fcdaa7c73a204952005691d
Leo Lee <nxl1969@gmail.com>
2017-02-02 23:59:47 -0500

    Add some comments comparing export default to export

--- weather/src/actions/index.js
+++ weather/src/actions/index.js
@@ -3,13 +3,16 @@ const API_KEY = '7ae59035c0dcb374826994830b6d6d11';
 const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`; //ES6 syntax
 //action creator always return a action
 export const FETCH_WEATHER = 'FETCH_WEATHER';
-export default function fetchWeather(city){
+//export without default means a "named export"
+//ex. import {fetchWeather} from ...
+//don't have to use {} when it's a export default
+export function fetchWeather(city){
 	
 	const url = `${ROOT_URL}&q=${city},us`;
 	const request = axios.get(url);
 	return {
 		type:FETCH_WEATHER,
 		payload:request
-	}
+	};
 	
 }
