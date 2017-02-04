b89e92c923f9b4e05cce85e8082358acee27cbce
Leo Lee <nxl1969@gmail.com>
2017-02-03 23:21:28 -0500

    Return a new instance of the state based on the action type

--- weather/src/reducers/reducer_weather.js
+++ weather/src/reducers/reducer_weather.js
@@ -1,4 +1,13 @@
+import {FETCH_WEATHER} from '../actions/index';
 export default function(state=null,action){
 	//console.log('action received:',action);
+	switch(type)
+	{
+		case FETCH_WEATHER:
+			//return a new instance of the state
+			//return state.concat([action.payload.data]); 
+			return [action.payload.data, ...state]; //ES6 syntax [city, city, city..]
+			break;
+	}
 	return state;
 }
