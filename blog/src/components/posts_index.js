bd882c52615ec49b5cdaa7e5f8e5d9c52c8e4b2b
Leo Lee <nxl1969@gmail.com>
2017-02-05 16:30:10 -0500

    Condensing the code by removing mapDispatchToProps and using ES6

--- blog/src/components/posts_index.js
+++ blog/src/components/posts_index.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
-import {bindActionCreators} from 'redux';
+//import {bindActionCreators} from 'redux';
 import {fetchPosts} from '../actions/index';
 class PostsIndex extends Component{
 	componentWillMount(){
@@ -13,7 +13,13 @@ class PostsIndex extends Component{
 	}
 }
 
-function mapDispatchToProps(dispatch){
+/*function mapDispatchToProps(dispatch){
 	return bindActionCreators({fetchPosts},dispatch);
 }
-export default connect(null, mapDispatchToProps)(PostsIndex);
+export default connect(null, mapDispatchToProps)(PostsIndex);*/
+
+/*shortcut for above code*/
+//export default connect(null, {fetchPosts: fetchPosts})(PostsIndex);
+
+/*ES6 syntax for condensing the code above*/
+export default connect(null,{fetchPosts}){PostsIndex}
