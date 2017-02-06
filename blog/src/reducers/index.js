1a07ac89c11cb4fae1b3f61f21a96c521f5a22e3
Leo Lee <nxl1969@gmail.com>
2017-02-06 00:08:00 -0500

    Import the reducer from redux-form Combine the formReducer with the other reducers

--- blog/src/reducers/index.js
+++ blog/src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import PostsReducer from './reducer_posts';
+import {reducer as formReducer} from 'redux-form'; //import redux form and grab the reducer property out of it and name it formReducer
 
 const rootReducer = combineReducers({
-  posts:PostsReducer
+  posts:PostsReducer, 
+  form:formReducer
 });
 
 export default rootReducer;
