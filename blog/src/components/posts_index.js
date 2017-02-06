1315d3dfc0fb2fa33879540c0541e2b0944c4e11
Leo Lee <nxl1969@gmail.com>
2017-02-05 23:32:39 -0500

    Import Link component from react-router Add a button for adding a post

--- blog/src/components/posts_index.js
+++ blog/src/components/posts_index.js
@@ -2,13 +2,20 @@ import React, {Component} from 'react';
 import {connect} from 'react-redux';
 //import {bindActionCreators} from 'redux';
 import {fetchPosts} from '../actions/index';
+import {Link} from 'react-router';
 class PostsIndex extends Component{
 	componentWillMount(){
 		this.props.fetchPosts();
 	}
 	render(){
 		return (
-			<div>List of blog posts</div>
+			<div>
+				<div className="text-xs-right">
+					<Link to="/posts/new" className="btn btn-primary">
+						Add a post
+					</Link>
+			    </div>
+			</div>
 		)
 	}
 }
