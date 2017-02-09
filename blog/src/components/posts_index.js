5a82415d6bc0a8f8e14e00d4e4d4c2251ae9bae1
Leo Lee <nxl1969@gmail.com>
2017-02-08 23:35:40 -0500

    Add a help function for rendering the posts

--- blog/src/components/posts_index.js
+++ blog/src/components/posts_index.js
@@ -7,6 +7,16 @@ class PostsIndex extends Component{
 	componentWillMount(){
 		this.props.fetchPosts();
 	}
+	renderPosts(){
+		return this.props.posts.map((post) =>{
+			return (
+				<li className="list-group-item" key={post.id}>
+					<span className="pull-xs-right">{post.categories}</span>
+					<strong>{post.title}</strong>
+				</li>
+			);
+		});
+	}
 	render(){
 		return (
 			<div>
@@ -15,11 +25,17 @@ class PostsIndex extends Component{
 						Add a post
 					</Link>
 			    </div>
+				<h3>Posts</h3>
+				<ul className="list-group">
+					{this.renderPosts()}
+				</ul>
 			</div>
 		)
 	}
 }
-
+function mapStateToProps(state){
+	return {posts: state.posts.all};
+}
 /*function mapDispatchToProps(dispatch){
 	return bindActionCreators({fetchPosts},dispatch);
 }
@@ -29,4 +45,5 @@ export default connect(null, mapDispatchToProps)(PostsIndex);*/
 //export default connect(null, {fetchPosts: fetchPosts})(PostsIndex);
 
 /*ES6 syntax for condensing the code above*/
-export default connect(null,{fetchPosts})(PostsIndex);
+//export default connect(null,{fetchPosts})(PostsIndex);
+export default connect(mapStateToProps,{fetchPosts})(PostsIndex);
